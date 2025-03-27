118707824b8924d8276a12edc5576b4a2c595ea5
PoonieWoonie <nicholas.hinglai.poon@gmail.com>
2025-03-27 12:09:14 -0400

    Finished!

--- tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-if (count1 != 179){
-            fprintf(sup8, "%i %lf\n", count1, savg);
-            fprintf(mich8, "%i %lf\n", count1, mavg);
-            fprintf(huron8, "%i %lf\n", count1, havg);
-            fprintf(erie8, "%i %lf\n", count1, eavg);
-            fprintf(on8, "%i %lf\n", count1, oavg);
-            fprintf(cl8, "%i %lf\n", count1, clavg);
-        }
